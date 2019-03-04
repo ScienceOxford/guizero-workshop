5c8db548ed194b61cc8fe97f407aa924e1946fb8
Sarah Townson <sarah.townson@scienceoxford.com>
2019-03-04 11:37:48 +0000

    Create starter.py

---
+++ starter.py
@@ -0,0 +1,11 @@
+from guizero import App, Text, PushButton, Slider, Picture, Window, error, TextBox
+
+app = App(title="Sarah's App")
+app.bg = "white"
+app.text_color = "black"
+app.width = 500
+app.height = 250
+
+text = Text(app, text="This is my app!", size=20)
+
+app.display()
