4fab83a052e69e2e301f64f38ff7d876e8daa660
Sarah Townson <sarah.townson@scienceoxford.com>
2019-03-04 12:34:22 +0000

    Create slider.py

---
+++ EXAMPLES/slider.py
@@ -0,0 +1,26 @@
+from guizero import App, Text, PushButton, Slider, Picture, Window, error, TextBox
+
+app = App(title="Sarah's App with slider")
+app.bg = "white"
+app.text_color = "black"
+app.width = 500
+app.height = 250
+
+def background_yellow():
+    app.bg = "yellow"
+    
+def background_green():
+    app.bg = "green"
+
+text = Text(app, text="This is my app, including slider!", size=20)
+button_1 = PushButton(app, text="Make it yellow!", command=background_yellow)
+button_2 = PushButton(app, text="Make it green!", command=background_green)
+
+# NEW CODE STARTS
+def colour_change():
+    app.bg = (red.value, 0, 0)
+
+red = Slider(app, end=255, command=colour_change)
+# NEW CODE ENDS
+
+app.display()
