99103b9b595b5bf7ef79cd89d1131beabaa03159
Sarah Townson <sarah.townson@scienceoxford.com>
2019-03-04 12:49:16 +0000

    Create textbox.py

---
+++ EXAMPLES/textbox.py
@@ -0,0 +1,27 @@
+from guizero import App, Text, PushButton, Slider, Picture, Window, error, TextBox
+
+app = App(title="Sarah's App with input")
+app.bg = "white"
+app.text_color = "black"
+app.width = 500
+app.height = 250
+
+def background_yellow():
+    app.bg = "yellow"
+    
+def background_green():
+    app.bg = "green"
+
+# NEW CODE STARTS
+def add_text():
+    new_text = Text(app, text=input_box.value)
+# NEW CODE ENDS
+
+text = Text(app, text="This is my app, including input!", size=20)
+button_1 = PushButton(app, text="Make it yellow!", command=background_yellow)
+button_2 = PushButton(app, text="Make it green!", command=background_green)
+
+# NEW CODE STARTS
+input_box = TextBox(app, width=app.width)
+button_3 = PushButton(app, text="Display the input", command=add_text)
+# NEW CODE ENDS
