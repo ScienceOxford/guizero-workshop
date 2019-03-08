a18823c288b864135308e22107f7e9be00146eaf
Sarah Townson <sarah.townson@scienceoxford.com>
2019-03-08 16:34:10 +0000

    Update textbox.py

--- EXAMPLES/textbox.py
+++ EXAMPLES/textbox.py
@@ -13,8 +13,9 @@ def background_green():
     app.bg = "green"
 
 # NEW CODE STARTS
-def add_text():
-    new_text = Text(app, text=input_box.value)
+def add_text():                                                         # creates a command called 'add_text'
+    new_text = Text(app, text=input_box.value)                          # which creates a Text widget that uses the value of
+                                                                        # the TextBox widget, created below, as the text
 # NEW CODE ENDS
 
 text = Text(app, text="This is my app, including input!", size=20)
@@ -22,6 +23,6 @@ button_1 = PushButton(app, text="Make it yellow!", command=background_yellow)
 button_2 = PushButton(app, text="Make it green!", command=background_green)
 
 # NEW CODE STARTS
-input_box = TextBox(app, width=app.width)
-button_3 = PushButton(app, text="Display the input", command=add_text)
+input_box = TextBox(app, width=app.width)                               # creates a TextBox widget, called 'input_box'
+button_3 = PushButton(app, text="Display the input", command=add_text)  # creates a PushButton widget, which calles the new command above
 # NEW CODE ENDS
